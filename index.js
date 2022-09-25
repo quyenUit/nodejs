7129a05b36e229b36e96fcc69b071a4b9dd00414
quyenUit <nguyenquyenboss@gmail.com>
2022-09-25 22:49:10 +0700

    update port on heroku

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 //cau hinh body-parser
 var bodyParser = require("body-parser");
+const bookRouter = require("./app/routers/book.router");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // cac router
@@ -9,7 +10,7 @@ require("./app/routers/home.router")(app);
 require("./app/routers/book.router")(app);
 
 // app.use("/", homeRouter);
-app.use("/", book.router);
+// app.use("/", bookRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
